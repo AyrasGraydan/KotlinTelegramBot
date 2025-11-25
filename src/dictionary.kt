0877a2ea4d3b3ea1076c6399af98961b271eec3f
AyrasGraydan <AyrasGraydan@gmail.com>
2025-11-25 18:31:59 +0300

    KTB-03-dictionary-parsing

--- src/dictionary.kt
+++ src/dictionary.kt
@@ -3,7 +3,20 @@ import java.io.File
 fun main() {
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello привет\ndog собака\ncat кошка")
+    wordsFile.writeText("hello|привет|0\ndog|собака|0\ncat|кошка")
 
-    wordsFile.readLines().forEach { println(it) }
+    val dictionary = mutableListOf<Word>()
+
+    wordsFile.readLines().forEach {
+        val lines = it.split("|")
+        dictionary.add(
+            Word(
+                lines[0],
+                lines[1],
+                lines.getOrNull(2)?.toInt() ?: 0
+            )
+        )
+    }
+
+    dictionary.forEach { println(it) }
 }
