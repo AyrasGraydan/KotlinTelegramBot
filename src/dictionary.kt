475f9f5b2df72e7b48a95cc8694f6ce15138f2be
AyrasGraydan <AyrasGraydan@gmail.com>
2025-11-27 07:13:54 +0300

    KTB-06-statistics

--- src/dictionary.kt
+++ src/dictionary.kt
@@ -1,18 +1,30 @@
 import java.io.File
 
+const val VALUE_OF_WORD_LEARNED = 3
+
 fun main() {
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello|привет|0\ndog|собака|0\ncat|кошка")
+    wordsFile.writeText(
+        "hello|привет|6\ndog|собака|0\ncat|кошка" +
+                "\nowl|сова|4\nsnake|змея|7\nrain|дождь|6"
+    )
 
     val dictionary = loadDictionary(wordsFile)
 
-    while(true) {
+    while (true) {
         println("Меню:\n1 – Учить слова\n2 – Статистика\n0 – Выход")
         print("Ваш выбор: ")
-        when(readln().toIntOrNull()){
+        when (readln().toIntOrNull()) {
             1 -> println("Выбор пункта: Учить слова")
-            2 -> println("Выбор пункта: Статистика")
+            2 -> {
+                val totalCount = dictionary.size
+                val learnedCount = dictionary
+                    .count { it.correctAnswersCount >= VALUE_OF_WORD_LEARNED }
+                val percent = 100 * learnedCount / totalCount
+                println("Выучено $learnedCount из $totalCount слов | $percent%")
+            }
+
             0 -> break
             else -> println("Такого вырианта нет")
         }
@@ -20,15 +32,13 @@ fun main() {
     }
 }
 
-fun loadDictionary(file: File): MutableList<Word>{
+fun loadDictionary(file: File): MutableList<Word> {
     val dictionary = mutableListOf<Word>()
     file.readLines().forEach {
         val line = it.split("|")
         dictionary.add(
             Word(
-                line[0],
-                line[1],
-                line.getOrNull(2)?.toInt() ?: 0
+                line[0], line[1], line.getOrNull(2)?.toInt() ?: 0
             )
         )
     }
