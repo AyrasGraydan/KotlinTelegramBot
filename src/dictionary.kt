2b8df007cacb7367140fc2af31103a586df11eda
AyrasGraydan <145202375+AyrasGraydan@users.noreply.github.com>
2025-12-05 21:31:47 +0300

    KTB-07-learning words

--- src/dictionary.kt
+++ src/dictionary.kt
@@ -1,11 +1,17 @@
 import java.io.File
 
 const val VALUE_OF_WORD_LEARNED = 3
+const val MAX_QUESTION_WORDS_SIZE = 4
+
+const val ORANGE = "\u001b[33m"
+const val BLUE = "\u001b[36m"
+const val GRAY = "\u001b[37m"
+const val RESET_COLOR = "\u001b[0m"
 
 fun main() {
     val wordsFile = File("words.txt")
 
-    if (!wordsFile.exists()){
+    if (!wordsFile.exists()) {
         wordsFile.createNewFile()
         fillFile(wordsFile)
     }
@@ -16,7 +22,36 @@ fun main() {
         println("Меню:\n1 – Учить слова\n2 – Статистика\n0 – Выход")
         print("Ваш выбор: ")
         when (readln().toIntOrNull()) {
-            1 -> println("Выбор пункта: Учить слова")
+            1 -> {
+                println()
+                println("${GRAY}Введите 0 для выхода ")
+
+                val notLearnedList = mutableListOf<Word>()
+                dictionary.forEach {
+                    if (it.correctAnswersCount < VALUE_OF_WORD_LEARNED)
+                        notLearnedList.add(it)
+                }
+
+                if (notLearnedList.isEmpty()) {
+                    println("Все слова выучены!")
+                } else {
+                    while (true) {
+                        val questionWords =
+                            notLearnedList.shuffled().take(MAX_QUESTION_WORDS_SIZE)
+                        val correctAnswer = questionWords.random()
+
+                        println()
+                        println("$BLUE${correctAnswer.original}")
+                        questionWords.forEachIndexed { index, word ->
+                            println("$ORANGE${index + 1}$RESET_COLOR - ${word.translate}")
+                        }
+                        print("Ваш выбор: ")
+                        if (readln().toIntOrNull() == 0)
+                            break
+                    }
+                }
+            }
+
             2 -> {
                 val totalCount = dictionary.size
                 val learnedCount = dictionary
@@ -47,7 +82,7 @@ fun loadDictionary(file: File): MutableList<Word> {
 
 fun fillFile(file: File) {
     file.writeText(
-        "hello|привет|6\ndog|собака|0\ncat|кошка" +
-                "\nowl|сова|4\nsnake|змея|7\nrain|дождь|6"
+        "hello|привет|1\ndog|собака|0\ncat|кошка" +
+                "\nowl|сова|2\nsnake|змея\nrain|дождь|6"
     )
 }
