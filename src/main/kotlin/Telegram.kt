8e4b0df8d153bf8c2d52a896fff66463aa66076a
AyrasGraydan <145202375+AyrasGraydan@users.noreply.github.com>
2025-12-24 21:16:01 +0300

    KTB-15-fix 2

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -5,15 +5,16 @@ fun main(args: Array<String>) {
         return
     }
     val telegramBotService = TelegramBotService()
+    val telegramParser = Parser()
     var lastUpdateId = 0
 
     while (true) {
         Thread.sleep(2000)
         val updates = telegramBotService.getUpdates(botToken, lastUpdateId)
 
-        lastUpdateId = telegramBotService.getLastUpdateId(updates) ?: lastUpdateId
-        val userMessage = telegramBotService.getUserMessage(updates)
-        val chatId = telegramBotService.getChatId(updates)
+        lastUpdateId = telegramParser.getLastInt(updates, regexUpdateId) ?: lastUpdateId
+        val userMessage = telegramParser.getFirstString(updates, regexUserMessage)
+        val chatId = telegramParser.getFirstLong(updates, regexChatId)
 
         if (userMessage.equals("hello", true) && chatId != null)
             telegramBotService.sendMessage(botToken, chatId, "Hello")
--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -4,14 +4,14 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-const val botApi = "https://api.telegram.org/bot"
+const val BOT_API = "https://api.telegram.org/bot"
 
 class TelegramBotService() {
 
     val httpClient: HttpClient = HttpClient.newBuilder().build()
 
     fun getUpdates(botToken: String, updateId: Int): String {
-        val urlGetUpdates = "$botApi$botToken/getUpdates?offset=$updateId"
+        val urlGetUpdates = "$BOT_API$botToken/getUpdates?offset=$updateId"
         val request = HttpRequest.newBuilder().uri(URI(urlGetUpdates)).build()
         val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
         return response.body()
@@ -19,23 +19,8 @@ class TelegramBotService() {
 
     fun sendMessage(botToken: String, chatId: Long, text: String) {
         val formattedText = URLEncoder.encode(text, "UTF-8")
-        val urlSendMessage = "$botApi$botToken/sendMessage?chat_id=$chatId&text=$formattedText"
+        val urlSendMessage = "$BOT_API$botToken/sendMessage?chat_id=$chatId&text=$formattedText"
         val request = HttpRequest.newBuilder().uri(URI(urlSendMessage)).build()
         httpClient.send(request, HttpResponse.BodyHandlers.ofString())
-    }
-
-    fun getUserMessage(updates: String): String? {
-        val matchText: MatchResult? = "\"text\":\"(.*?)\"".toRegex().find(updates)
-        return matchText?.groupValues?.get(1)
-    }
-
-    fun getLastUpdateId(updates: String): Int? {
-        val matchUpdate = "\"update_id\":(\\d+)".toRegex().findAll(updates)
-        return matchUpdate.lastOrNull()?.let { it.groupValues.getOrNull(1)?.toInt()?.plus(1) }
-    }
-
-    fun getChatId(updates: String): Long? {
-        val matchChatId: MatchResult? = "\"chat\":\\{\"id\":(\\d+)".toRegex().find(updates)
-        return matchChatId?.groupValues?.get(1)?.toLong()
     }
 }
